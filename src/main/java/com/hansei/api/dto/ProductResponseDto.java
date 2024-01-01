809175f0a9c52aed25ea4aaadf65c391fcba666e
bjkang <kaq6822@naver.com>
2024-01-01 23:50:07 +0900

    fix: wrong type fix

--- src/main/java/com/hansei/api/dto/ProductResponseDto.java
+++ src/main/java/com/hansei/api/dto/ProductResponseDto.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  * DTO for {@link com.hansei.api.entity.Product}
  */
 public record ProductResponseDto(Long productId, String productName, String productImageUrl,
-                                 String productPrice) implements Serializable {
+                                 Long productPrice) implements Serializable {
     public ProductResponseDto(Product product) {
         this(product.getProductId(), product.getProductName(), product.getProductImageUrl(), product.getProductPrice());
     }
