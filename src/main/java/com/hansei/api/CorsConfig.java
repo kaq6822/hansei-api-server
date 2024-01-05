285372439bc2d406f76a5ccc56dd87d0a3fc7bd5
bjKang <kaq6822@naver.com>
2024-01-05 13:25:48 +0900

    feat: Cors config setup

---
+++ src/main/java/com/hansei/api/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.hansei.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
